3e66bab64f0e5e4170506b18215ac82efb91c6fa
CHEN Xian-an <xianan.chen@gmail.com>
2025-09-16 14:41:57 +0800

    Add face-box patch

--- Formula/emacs-plus@31.rb
+++ Formula/emacs-plus@31.rb
@@ -92,6 +92,7 @@ class EmacsPlusAT31 < EmacsBase
   opoo "The option --with-no-frame-refocus is not required anymore in emacs-plus@31." if build.with? "no-frame-refocus"
   local_patch "system-appearance", sha: "53283503db5ed2887e9d733baaaf80f2c810e668e782e988bda5855a0b1ebeb4"
   local_patch "round-undecorated-frame", sha: "26947b6724fc29fadd44889808c5cf0b4ce6278cf04f46086a21df50c8c4151d"
+  local_patch "face-box-line-width-4dirs", sha: "e3ff9a199ab242f716a68e959a5d428822630f5074d9ef3c2896d5c81a21e24a"
 
   #
   # Install
